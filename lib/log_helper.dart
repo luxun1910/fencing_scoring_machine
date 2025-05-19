876601c2915dab85e6ac937b38416d1bc5d46608
luxun1910 <luxun.unanimity1910@gmail.com>
2025-05-19 17:22:33 +0800

    made log_helper singleton

--- lib/log_helper.dart
+++ lib/log_helper.dart
@@ -1,3 +1,17 @@
 import 'package:logger/logger.dart';
 
-final logger = Logger();
+final logger = LogHelper().logger;
+
+class LogHelper {
+  static final LogHelper _instance = LogHelper._internal();
+  final Logger logger = Logger();
+
+  factory LogHelper() {
+    return _instance;
+  }
+
+  // シングルトンにするため、コンストラクタをprivateにする
+  LogHelper._internal();
+
+  static Logger get instance => _instance.logger;
+}
